017eaf1af0eb50bcbe75a84926c3d0e3dc062c0e
mung3477 <mung3477@naver.com>
2024-12-13 08:01:28 +0000

    feat: test script

--- inference_loop.sh
+++ inference_loop.sh
@@ -20,19 +20,28 @@ END
 
 
 # alphas=(0.0 0.2 0.4 0.6 0.8 1.0)
-alphas=(1.0)
-for alpha in "${alphas[@]}"
+# for alpha in "${alphas[@]}"
+
+subjects=("girl with a purse in anime style" "A man with short hair" "High-heeled shoe encrusted with diamonds")
+subj_phrases=("girl purse anime" "man short hair" "High-heeled shoe encrusted")
+ref=("hulk.png" "long hair woman.png" "shoes.png")
+cntl=("depth" "canny" "canny")
+cond_prompts=("a photo of hulk" "A woman with long hair" "Enamel ankle boots with a strap")
+cond_phrases=("photo hulk" "woman long hair" "ankle boots strap")
+
+for index in "${!subjects[@]}"
 do
 	CUDA_VISIBLE_DEVICES="3" python3 smartcontrol_demo.py \
-		--prompt="a photo of tiger" \
-		--gen_phrase="a photo of tiger" \
-		--ref="deer.png" \
-		--cond_prompt="a photo of deer" \
-		--cond_phrase="a photo of deer" \
-		--cntl="depth" \
+		--prompt="${subjects[$index]}" \
+		--gen_phrase="${subj_phrases[$index]}" \
+		--ref="${ref[$index]}" \
+		--cond_prompt="${cond_prompts[$index]}" \
+		--cond_phrase="${cond_phrases[$index]}" \
+		--cntl="${cntl[$index]}" \
 		--seed=12345 \
-		--alpha_mask=$alpha \
-		--alpha_attn_diff
+		--alpha_mask=1 \
+		--alpha_attn_diff \
+		--ignore_special_tkns
 
 
 done
