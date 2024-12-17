e54b0823d6713c12b77a90420daeb5fc15e96a81
mung3477 <mung3477@naver.com>
2024-12-17 05:53:41 +0000

    fix: post mortem supports special token ignorance

--- post_mortem.sh
+++ post_mortem.sh
@@ -1,6 +1,6 @@
 # visualize attention map difference
 python3 post_mortem.py \
 	--attn_diff \
-	--diff_layer="./log/attn_maps/smartcontrol-High-heeled shoe encrusted with diamonds-canny-shoes-control-1.0-alpha-diff-High-heeled shoe diamonds vs Enamel boots-no <sot> <eot>-seed-12345/999/ControlNetModel.down_blocks.1.attentions.0.transformer_blocks.0.attn2/batch-0" \
-	--gen_phrase="High-heeled shoe diamonds" \
-	--cond_phrase="Enamel ankle boots with a strap"
+	--diff_layer="./log/attn_maps/smartcontrol-A photo of tiger-depth-deer-control-1.0-alpha-diff-A photo of tiger vs A photo of deer-threshold 0.2-seed-12345/999/ControlNetModel.down_blocks.1.attentions.1.transformer_blocks.0.attn2/batch-0" \
+	--gen_phrase="A photo of tiger" \
+	--cond_phrase="A photo of deer"
